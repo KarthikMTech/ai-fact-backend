9106f9ab9da06ffb39fc7fd05e947a448fb595ce
KarthikMTech <karthikeyan.mtech@hotmail.com>
2025-07-20 14:25:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import json
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Check and set your Gemini API key
 api_key = os.getenv("GEMINI_API_KEY")
